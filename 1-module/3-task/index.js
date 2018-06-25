6efed0df4c133d3960a1a1ed1ed7eb9bfaf3b937
egrashina.gi <10Fev1989Gal>
2018-06-25 16:06:26 +0300

    Tasks of module 1 - done!

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -5,7 +5,17 @@
  * @param   {string} str -  входные данные
  * @returns {{min:number, max:number}}  объект
  */
-function getMinMax(str) {
 
+let inputData = '1, -5.8 или 10, хотя 34 + -5.3 и 73';
+
+function getMinMax(string) {
+    let stepOne = string.replace(/[^a-zA-Z0-9!.-]/g, ' ');
+    let stepTwo = stepOne.replace(/\s+/g, ',');
+    let newArr = stepTwo.split(',');
+
+    let min = Math.min(...newArr);
+    let max = Math.max(...newArr);
+    return {min: min, max: max}
 }
 
+console.log(getMinMax(inputData));
